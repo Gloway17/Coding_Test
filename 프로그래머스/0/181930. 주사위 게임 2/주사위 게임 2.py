9c786868c09ae83ed642fa6159bba32a1ee69465
Gloway17 <96361533+Gloway17@users.noreply.github.com>
2025-04-16 20:01:56 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,10 +1,12 @@
 def solution(a, b, c):
     answer = 0
     
-    if a == b and a == c:
-        answer = (a+b+c)*(a**2 + b**2 + c**2)*(a**3 + b**3 + c**3)
-    elif a==b or a==c or b==c:
-        answer = (a+b+c)*(a**2 + b**2 + c**2)
+    l = list(set([a, b, c]))
+    
+    if len(l) == 1:
+        answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 )
+    elif len(l) == 2:
+        answer = (a + b + c) * (a**2 + b**2 + c**2 )
     else:
         answer = a + b + c
     return answer
