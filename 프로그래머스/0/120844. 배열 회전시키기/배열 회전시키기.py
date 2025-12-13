1e658620441294624a9423e2d7ab45a674994743
Gloway17 <96361533+Gloway17@users.noreply.github.com>
2025-12-13 14:36:56 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,11 @@
+def solution(numbers, direction):
+    answer = []
+    
+    if (direction == 'left'):
+        answer = numbers[1:]
+        answer.append(numbers[0])
+    else:
+        answer.append(numbers[-1])
+        answer.extend(numbers[:-1])
+    
+    return answer
